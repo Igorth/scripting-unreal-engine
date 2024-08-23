94f6e3fec1b82724406640a254ee35ca40aed0c7
igord <igordiasth@gmail.com>
2024-08-23 16:03:56 -0400

    add `get_static_mesh_data

--- Content/Python/northwood_script.py
+++ Content/Python/northwood_script.py
@@ -42,12 +42,36 @@ def get_asset_class(class_type):
     for asset_path in asset_paths:
         asset_data = EAL.find_asset_data(asset_path)
         asset_class = asset_data.asset_class
+        # unreal.log(f"Asset Class: {asset_class}")  # Verify asset class name
         if asset_class == class_type:
             assets.append(asset_data.get_asset())
 
-    for asset in assets:
-        unreal.log(f"Assets: {asset}")
+    # for asset in assets:
+    #     unreal.log(f"Assets: {asset}")
+
+    return assets
 
 
+def get_static_mesh_data():
+    static_meshes = get_asset_class('StaticMesh')
+    
+    for static_mesh in static_meshes:
+        # asset_import_data = static_mesh.get_editor_property('asset_import_data')
+        # fbx_file_path = asset_import_data.extract_filenames()
+        # unreal.log(f"Static Mesh: {fbx_file_path}")
+        lod_group_info = static_mesh.get_editor_property('lod_group')
+        unreal.log(f"LOD Group: {lod_group_info}") 
 
-get_asset_class('Texture2D')
+        if lod_group_info == 'None':
+            if static_mesh.get_num_lods() == 1:
+                static_mesh.set_editor_property('lod_group', 'LargeProp')
+
+
+def get_textures_twod():
+    textures = get_asset_class('Texture2D')
+
+    for texture in textures:
+        texture_srgb = texture.get_editor_property('srgb')
+        unreal.log(f"Texture sRGB: {texture_srgb}")
+
+get_static_mesh_data()
